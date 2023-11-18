02ce3ba8b2141dce12389812d352e8b0e2f6b18c
Adel <elhabbal6432@saskpolytech.ca>
2023-11-18 16:32:21 -0600

    Text is added after pressing submit. (Submit Test)

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process_input', methods=['POST'])
+def process_text():
+    keyword = request.form['keyword']
+    location = request.form['location']
+    processed_text = f"You entered: {keyword} and {location}"
+    return jsonify(result=processed_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
